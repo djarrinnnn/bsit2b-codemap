4f5372af8bd71648db4a7f5499f5ce992a8259d1
djarrinnnn <uatuthewatcher73@gmail.com>
2025-08-20 05:28:34 +0800

    Add Spring Boot application entry point

---
+++ src/main/java/com/example/bsit2b/CodemapApplication.java
@@ -0,0 +1,19 @@
+package com.example.bsit2b;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@SpringBootApplication
+@Controller
+public class CodemapApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CodemapApplication.class, args);
+    }
+
+    @GetMapping("/")
+    public String home() {
+        return "index";
+    }
+}
